5bed7af0b585557ad83ed29a9d475e0d51090f19
Satya Ranjan <satyaranjan@Satyas-Mac-mini.lan>
2024-08-19 17:39:48 +0530

    chnages

--- ques4.js
+++ ques4.js
@@ -2,27 +2,27 @@
 
 
 
-
+let nums = [2, 2, 1, 1, 1, 2, 2];
 function maximum(nums) {
     let count = 0;
-    let c = 0;
+    let z = 0;
 
     for (let i = 0; i < nums.length; i++) {
-        let num = nums[i];
+        let b = nums[i];
 
         if (count === 0) {
-            c = num;
+            z = b;
         }
 
-        if (num === c) {
+        if (b === z) {
             count = count + 1;
         } else {
             count = count - 1;
         }
     }
 
-    return c;
+    return z;
 }
 
-let array = [2, 2, 1, 1, 1, 2, 2];
-console.log(maximum(array));
+// let nums = [2, 2, 1, 1, 1, 2, 2];
+console.log(maximum(nums));

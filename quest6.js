6c58671b2ba854f25d8faf88210b32dfa08fd847
satya1105 <satya.ranjan.vit@gmail.com>
2024-08-24 23:09:30 +0530

    asd

--- quest6.js
+++ quest6.js
@@ -1,17 +1,19 @@
-let s = "s,a,t,y,a"
-let t = "s,a,t,y,a"
+let s = "s,a,t,y,a";
+let t = "s,a,t,a,y";
 
-function anagram(s,t){
+function anagram(s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
 
-    if(s.length !== t.length){ 
+    let a = s.split("").sort().join("");
+    let b = t.split("").sort().join("");
 
-     a= s.split("").sort().join("")
-     b= t.split("").sort().join("")
-     if(a === b){
-         return true
-     }
-
-    }else {
-        return false
+    if (a === b) {
+        return true;
+    } else {
+        return false;
     }
 }
+
+console.log(anagram(s, t));
